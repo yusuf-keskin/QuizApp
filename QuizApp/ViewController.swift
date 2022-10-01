5cef03c95495653926b403a66b45f80b1ed0465d
Yusuf Keskin <yusuf_keskin@outlook.com>
2022-10-01 15:56:37 +0300

    Initial Commit

---
+++ QuizApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by YUSUF KESKİN on 1.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
